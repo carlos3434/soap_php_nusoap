94e11ef8f86cbe63bddd2c724fe563cd80b66f13
Juan Rojas <carlos34343434@gmail.com>
2015-09-01 15:58:25 -0500

    añadiendo wsdl y definicion de parametros

--- servicio.php
+++ servicio.php
@@ -3,8 +3,33 @@
 require_once('lib/nusoap.php');
 require_once('calculadora.php');
 
-$server = new nusoap_server();
-$server->register('calculadora');
+$server = new soap_server();
+$server->configureWSDL("psi", "urn:psi");
+$server->soap_defencoding = 'UTF-8';
 
-$HTTP_RAW_POST_DATA = isset($HTTP_RAW_POST_DATA) ? $HTTP_RAW_POST_DATA : '';
-$server->service($HTTP_RAW_POST_DATA);
+$server->wsdl->addComplexType(
+    'input', 
+    'complexType', 
+    'struct', 
+    'all', 
+    '',
+    array(
+        'x'   => array('name' => 'x','type' => 'xsd:string'),
+        'y'    => array('name' => 'y','type' => 'xsd:string'),
+        'operacion'    => array('name' => 'operacion','type' => 'xsd:string')
+    )
+);
+//$server->register('calculadora');
+$server->register(
+    'calculadora',
+    //array('input' => 'tns:input'), 
+    array('x' => 'xsd:string','y' => 'xsd:string','operacion' => 'xsd:string'),
+    array("return" => "xsd:string"),
+    'urn:psi',   //namespace
+    'urn:psi#calculadora',  //soapaction
+    'rpc', // style
+    'encoded', // use
+    'operaciones basicas'
+);
+@$server->service($HTTP_RAW_POST_DATA);
+
